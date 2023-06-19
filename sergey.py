5adda270aa104834aadc039714c1416f8ac0cd64
DSSerg <134340086+DSSerg@users.noreply.github.com>
2023-06-20 02:09:11 +0300

    Update sergey.py

--- sergey.py
+++ sergey.py
@@ -28,10 +28,8 @@ st.subheader('Введенные параметры')
 st.write(df)
 
 #загрузка модели
-#pickle_in = open('model.pkl', 'rb')
-#clust = pickle.load(open("https://drive.google.com/file/d/1oUBUar4jsVNr9VnNNT0LAtkZfdzWprjs/view?usp=sharing", "rb"))
 clust = pickle.load(open("model1.pkl", "rb"))
 result=clust.predict(df)
 
-st.write("Клиент относится к кластеру:", float(result[0]))
+st.write("Клиент относится к кластеру:", result[1]))
 
