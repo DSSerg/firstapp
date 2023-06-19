daea4d566661a53e457ac1a8e18e86eeedb56a83
DSSerg <134340086+DSSerg@users.noreply.github.com>
2023-06-20 02:02:46 +0300

    Update sergey.py

--- sergey.py
+++ sergey.py
@@ -15,9 +15,9 @@ st.sidebar.header('Ввод параметров')
 def user_input_features():
     gender = st.sidebar.slider('Gender (0 - female, 1 - male)', 0, 1, 1)
     age = st.sidebar.slider('Age', 14, 70, 35)
-    trans = st.sidebar.slider('Transactions', 0, 50000, 1000)
+    trans = st.sidebar.slider('Transactions', 0.0, 70000.0, 1000)
     data = {'CustGender': gender,
-            'TransactionAmount (BYN)': (float) trans,
+            'TransactionAmount (BYN)': trans,
             'Age': age}
     features = pd.DataFrame(data, index=[0])
     return features
@@ -30,7 +30,7 @@ st.write(df)
 #загрузка модели
 #pickle_in = open('model.pkl', 'rb')
 #clust = pickle.load(open("https://drive.google.com/file/d/1oUBUar4jsVNr9VnNNT0LAtkZfdzWprjs/view?usp=sharing", "rb"))
-clust = pickle.load(open("model.pkl", "rb"))
+clust = pickle.load(open("model1.pkl", "rb"))
 result=clust.predict(df)
 
 st.write("Клиент относится к кластеру:", result)
