d9e568f40de73b1fb67a405074fff3c0cb742e3a
DSSerg <134340086+DSSerg@users.noreply.github.com>
2023-06-20 00:17:56 +0300

    Update sergey.py

--- sergey.py
+++ sergey.py
@@ -28,6 +28,6 @@ st.write(df)
 
 #загрузка модели
 #pickle_in = open('model.pkl', 'rb')
-clust = pickle.loads(open('/app/firstapp/model.pkl', 'rb'))
+clust = pickle.load(open("/app/firstapp/model.pkl", "rb"))
 
 
